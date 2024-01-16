8c350d38efa5550c39f928fe9ad9ab81027ae7bb
guan-navy <2693459705@qq.com>
2024-01-16 15:09:32 +0800

    axios的基础封装

--- 黑马/小兔鲜电商/vue-rabbit/src/main.js
+++ 黑马/小兔鲜电商/vue-rabbit/src/main.js
@@ -6,6 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
+//测试接口函数
+import {getCategpry}from '@/apis/testAPI'
+getCategpry().then((res)=>{
+  console.log(res)
+})
 const app = createApp(App)
 
 app.use(createPinia())
