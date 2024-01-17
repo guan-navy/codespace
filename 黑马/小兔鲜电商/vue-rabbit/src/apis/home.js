ffdbe8ae21b0d8ddfa69cc568d269312e7b4c2ec
guan-navy <2693459705@qq.com>
2024-01-17 15:54:36 +0800

    插槽的使用

---
+++ 黑马/小兔鲜电商/vue-rabbit/src/apis/home.js
@@ -0,0 +1,14 @@
+import httpInstance from '@/utils/http'
+
+// 获取banner
+ export function getBannerAPI() {
+  return httpInstance({
+    url:'home/banner'
+  })
+}
+
+export const findNewAPI = () => {
+    return httpInstance({
+      url:'/home/new'
+    })
+  }
--- 黑马/小兔鲜电商/vue-rabbit/src/views/Home/index.vue
+++ 黑马/小兔鲜电商/vue-rabbit/src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,12 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <!-- 测试面板组件 -->
+  <HomePanel title="新鲜好物" sub-title="新鲜好物好多商品">
+    <div>我是新鲜好物的插槽</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐好多商品">
+    <div>我是人气推荐的插槽</div>
+  </HomePanel>
 </template>
