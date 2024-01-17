b7f185c8a836377caf66b9e5e64658a2059b4b92
guan-navy <2693459705@qq.com>
2024-01-17 21:16:25 +0800

    状态栏的点击高亮,页面的分类展示

--- 黑马/小兔鲜电商/vue-rabbit/src/views/Layout/components/LayoutFixed.vue
+++ 黑马/小兔鲜电商/vue-rabbit/src/views/Layout/components/LayoutFixed.vue
@@ -15,10 +15,10 @@ const category = usecategoryStore()
       <!-- 导航区域 -->
       <ul class="app-header-nav ">
         <li class="home">
-          <RouterLink to="/">首页</RouterLink>
+          <RouterLink exact-active-class="active" to="/">首页</RouterLink>
         </li>
         <li class="home" v-for="item in category.categoryList" :key="item.id">
-          <RouterLink :to= " `/category/${item.id}` ">{{ item.name }}</RouterLink>
+          <RouterLink active-class="active" :to= " `/category/${item.id}` ">{{ item.name }}</RouterLink>
         </li>
         
       </ul>
--- 黑马/小兔鲜电商/vue-rabbit/src/views/Layout/components/LayoutHeader.vue
+++ 黑马/小兔鲜电商/vue-rabbit/src/views/Layout/components/LayoutHeader.vue
@@ -14,10 +14,10 @@ const category = usecategoryStore()
             </h1>
             <ul class="app-header-nav">
                 <li class="home" >
-          <RouterLink to="/">首页</RouterLink>
+          <RouterLink exact-active-class="active" to="/">首页</RouterLink>
         </li>
                 <li class="home" v-for="item in category.categoryList" :key="item.id">
-          <RouterLink :to= " `/category/${item.id}` ">{{ item.name }}</RouterLink>
+          <RouterLink active-class="active" :to= " `/category/${item.id}` ">{{ item.name }}</RouterLink>
         </li>
                 
             </ul>
