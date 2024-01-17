2168ea706f8ef9b6f689cc308e111cbf82764035
guan-navy <2693459705@qq.com>
2024-01-17 16:38:28 +0800

    图片懒加载

--- 黑马/小兔鲜电商/vue-rabbit/src/apis/home.js
+++ 黑马/小兔鲜电商/vue-rabbit/src/apis/home.js
@@ -12,3 +12,14 @@ export const findNewAPI = () => {
       url:'/home/new'
     })
   }
+
+  /**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+    return  httpInstance(  {
+        url:'home/hot'
+    })
+  }
