27a4cdad3f34ed1f8342bd954b1d9ac8c6c7fd2d
guan-navy <2693459705@qq.com>
2024-10-27 10:46:01 +0800

    'nest'

--- react/b站/基础知识/组件库/antd/src/App.js
+++ react/b站/基础知识/组件库/antd/src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Table } from "antd";
 
 function App() {
+  const dataSource = [
+    {
+      key: "1",
+      name: "胡彦斌",
+      age: 32,
+      address: "西湖区湖底公园1号",
+    },
+    {
+      key: "2",
+      name: "胡彦祖",
+      age: 42,
+      address: "西湖区湖底公园1号",
+    },
+    {
+      key: "3",
+      name: "张三",
+      age: 42,
+      address: "翻斗花园1号",
+    },
+
+    {
+      key: "4",
+      name: "李四",
+      age: 42,
+      address: "翻斗花园1号",
+    },
+    {
+      key: "5",
+      name: "王五",
+      age: 42,
+      address: "翻斗花园1号",
+    },
+    
+
+  ];
+
+  const columns = [
+    {
+      title: "姓名",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "年龄",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "住址",
+      dataIndex: "address",
+      key: "address",
+      render: (text) => <a href="URL_ADDRESS">{text}</a>,
+    },
+    {
+      title: "操作",
+      key: "action",
+
+      render: (text, record) => {
+        console.log('record',record)
+        console.log('text',text)
+        return(<span>
+          <Button>删除</Button>
+          <Button>编辑</Button>
+        </span>)
+        
+      },
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Button type="primary">Primary</Button>
+      <Table dataSource={dataSource} columns={columns} />;
     </div>
   );
 }
--- react/b站/基础知识/组件库/antd/src/index.js
+++ react/b站/基础知识/组件库/antd/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ConfigProvider } from 'antd';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: 'pink',
+        },
+      }}
+    >
     <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
